025ed8b531c2e709c95d73285c3fba5875682e0b
poornaChandra2629 <31150761+poornaChandra2629@users.noreply.github.com>
2020-05-11 15:58:54 +0530

    Add files via upload
    Code for Beginners of spring boot

---
+++ DependencyInjection-1/src/main/java/com/example/demo/DependencyInjection1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class DependencyInjection1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjection1Application.class, args);
+	}
+
+}
